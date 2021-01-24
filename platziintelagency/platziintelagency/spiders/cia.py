73832df078822ef323b5283689007715d0d90512
Juedquipa <Juedquipa29@gmail.com>
2021-01-23 23:25:18 -0500

    Scraper finalizado

--- platziintelagency/platziintelagency/spiders/cia.py
+++ platziintelagency/platziintelagency/spiders/cia.py
@@ -1,6 +1,8 @@
 import scrapy
 
 # Links //div[@class="field-item even"]/*[self::h2 or self::h3]/a/@href
+# Doctitles //h1[@class="documentFirstHeading"]/text()
+# Docp //div[@class="field-item even"]//p[not(@class)]/text()
 
 class SpiderCIA(scrapy.Spider):
 
@@ -18,7 +20,15 @@ class SpiderCIA(scrapy.Spider):
     def parse(self, response):
         links_declassified = response.xpath('//div[@class="field-item even"]/*[self::h2 or self::h3]/a/@href').getall()
         for link in links_declassified:
-            yield response.follow(link, callback=self.parse_link)
+            yield response.follow(link, callback=self.parse_link, cb_kwargs={'url': response.urljoin(link)})
 
-    def parse_link(self, response):
-        pass
+    def parse_link(self, response, **kwargs):
+        link = kwargs['url']
+        title = response.xpath('//h1[@class="documentFirstHeading"]/text()').get()
+        paragraph = response.xpath('//div[@class="field-item even"]//p[not(@class)]/text()').getall()
+
+        yield{
+            'url': link,
+            'tittle': title,
+            'body': paragraph
+        }
